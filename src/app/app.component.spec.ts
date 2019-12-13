add9367702e428628f3354827172986d27b6efd5
Julien Catania <julien.catania@gmail.com>
2019-12-13 11:45:35 +0100

    mocking http call with HttpClientTestingModule \o/

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,19 +1,25 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
-import {HttpClientModule} from '@angular/common/http';
+import {TestBed, async} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from './app.component';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
+
+  let httpTestingController: HttpTestingController;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
-        HttpClientModule
+        HttpClientTestingModule
       ],
       declarations: [
         AppComponent
       ],
     }).compileComponents();
+
+    httpTestingController = TestBed.get(HttpTestingController);
+
   }));
 
   it('should create the app', () => {
@@ -23,11 +29,23 @@ describe('AppComponent', () => {
   });
 
   it('Connexion should be ok', () => {
+    const userName = 'Bret';
+
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
 
     expect(app.logged).toBeFalsy();
-    app.connection('Bret');
+    // making http call with the wanted testing method
+    app.connection(userName);
+
+    // creating the mock
+    const req = httpTestingController.expectOne('https://jsonplaceholder.typicode.com/users?username=' + userName);
+    // respond the http call made in line 39
+    req.flush({});
+    // Finally, assert that there are no outstanding requests.
+    httpTestingController.verify();
+
+    // my expect
     expect(app.logged).toBeTruthy();
   });
 });
